ca31c39197f847c50461ed603b488d5901eff9d8
Victor Bergmann <vic.kunde@gmail.com>
2020-11-05 15:54:51 -0300

    Controller and Main advanced

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,45 +1,81 @@
 package sample;
 
+
+import javax.swing.plaf.basic.BasicBorders;
+import java.util.HashMap;
+
 public class Controller {
 
-    /*
-    reg SP, PC
+    private Short stackPointer, ProgramCounter;
+    private HashMap<Integer, Integer> instructionTable;
 
+    Controller(){
+        stackPointer = 512;
+        ProgramCounter = 0;
 
+        instructionTable = new HashMap<>();
+        instructionTable.put(0, 21);
+        instructionTable.put(1, 21);
+        instructionTable.put(2, 11);
+        instructionTable.put(3, 31);
+        instructionTable.put(4, 21);
+        instructionTable.put(5, 21);
+        instructionTable.put(6, 11);
+        instructionTable.put(7, 01);
+        instructionTable.put(8, 01);
+        instructionTable.put(10,11);
+        instructionTable.put(11, 00);
+        instructionTable.put(12, 01);
+        instructionTable.put(13, 30);
+        instructionTable.put(14, 11);
+        instructionTable.put(15, 01);
+        instructionTable.put(16, 00);
+    }
 
-    */
-
-    int stackPointer, ProgramCounter;
-
-
-    public int getStackPointer() {
+    public Short getStackPointer() {
         return stackPointer;
     }
-    public void setStackPointer(int stackPointer) {
+    public void setStackPointer(Short stackPointer) {
         this.stackPointer = stackPointer;
     }
-    public int getProgramCounter() {
+    public Short getProgramCounter() {
         return ProgramCounter;
     }
-    public void setProgramCounter(int programCounter) {
+    public void setProgramCounter(Short programCounter) {
         ProgramCounter = programCounter;
     }
+    public void incrementProgramCounter(){ProgramCounter++; }
 
-    private int oppCodeDecoder(short oppCode){
+    int oppCodeDecoder(Short oppCode){
+        return instructionTable.get(oppCode);
+    }
+
+    public void operation(Short oppCode){
+        //TODO
+
+    }
+    public void operation(Short oppCode, Short ACC, Short MAR){
         switch (oppCode){
-            //TODO
+            case 00:
+                ProgramCounter = MAR;
+                break;
+            case 05://negative
+                if(ACC < 0){
+                    ProgramCounter = MAR;
+                }
+                break;
+            case 01://positive
+                if(ACC > 0){
+                    ProgramCounter = MAR;
+                }
+                break;
+            case 04://zero
+                if(ACC == 0){
+                    ProgramCounter = MAR;
+                }
+                break;
         }
-        return 0;
-    }
-    public void operation(short oppCode){
-        //TODO
-    }
-    public void operation(short oppCode, short oper){
-        //TODO
 
-    }
-    public void operation(short oppCode, short oper1, short oper2){
-        //TODO
     }
 
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,8 +1,80 @@
 package sample;
 
-public class Main{
+public class Main {
+
+//inicializa memoria
+//inicializa CPU
+//INICIALIZA O CARREGADOR
+//RODA O CARREGADOR
+
+
+    ULA ula;
+    Controller controller;
+    Memory memory;
+    Short IR , MAR, ACC;
+    byte MOP;
+    int numOpp;
+
+
+    public Main(){
+        ula = new ULA();
+        controller = new Controller();
+        memory = new Memory();
+        IR = 0;
+        MAR = 0;
+        ACC = 0;
+    }
+
+    private void Program() {
+
+
+        while (IR != 11) /*oppCode do stop  */ {
+
+            IR = memory.getData(controller.getProgramCounter());
+            numOpp = controller.oppCodeDecoder(IR);
+
+            switch (numOpp) {
+                case 11:
+                    MAR = memory.getData(controller.getProgramCounter() + 1);
+                    ACC = ula.operation(IR, ACC, MAR);
+                    break;
+                case 21:
+                    controller.incrementProgramCounter();
+                    MAR = memory.getData(controller.getProgramCounter() + 1);
+                    controller.operation(IR, ACC, MAR);
+                    break;
+                case 30:// copy
+                    break;
+                case 31://load
+                    break;
+                default:
+                    break;
+
+            }
+            if(!(numOpp == 20))  controller.incrementProgramCounter();
+        }
+
+
+        // CPU = new CPU();
+        // = 1
+        // WHILE{
+        //
+        //   ATUALIZA RI
+        //   MANDA RI PRO CONTROLER
+        //   CONTROLER RETORNA QUANTOS OPERANDOS
+        //   MANDA PRO CONTROLLER (RI, OPERANDOS)
+        //   BUSCA O OPERANDO
+        //   CONTROLLER CHAMA A OPERAÇÃO
+        //   ULA EXECUTA A OPERAÇÃO
+        //   ATUALIZA PC
+        //
+
+        System.out.println("hello world!");
+
+    }
 
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        Main main = new Main();
+        main.Program();
     }
 }
