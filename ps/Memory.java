939c12da936f1b0d586fcece0a7fd6a742c8618c
Guilherme1999 <48689821+Guilherme1999@users.noreply.github.com>
2020-11-07 14:26:35 -0300

    Update Memory.java

--- ps/Memory.java
+++ ps/Memory.java
@@ -1,43 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ps;
 
 import java.util.ArrayList;
 
 public class Memory {
+    private ArrayList<String> mem;
+    private int dataPointer;
 
- private ArrayList<String> mem;
- private int dataPointer;
 
+    public Memory(){
+        mem = new ArrayList<String>();
+        for(int i=0;i<512;i++)
+            mem.add("0");
+        
+        mem.set(13,"0000000001000010000000000001111"); //add #15 
+        mem.set(14,"0000000001001110000000000000010"); //mult #2
+        mem.set(15,"0000000000000111000000000011111"); //store 31
+        mem.set(16,"00000000010011010000000001000000000000000001111"); //copy 32, 15
+        mem.set(17,"0000000000001011"); // stop
 
- public Memory(){
-  mem = new ArrayList<String>();
-  
-  for(int i=0;i<512;i++)
-      mem.add("0");
-  
-  mem.set(13,"0000100000000010000000000001111"); //add #15 0000.0000.0000.0010./0000.1000.0000.1010
-  mem.set(14,"0000000000000111000000000011111"); //store 31
-  mem.set(15,"0000000000001011"); // stop
- 
- }
+    }
 
- public String getData(int position) {     
-  return (mem.get(position));
- }
+    public String getData(int position) {     
+        return (mem.get(position));
+    }
 
- public void setData(int position, String data) {
-  mem.set(position, data);
- }
-
- public void setDataPointer(int dataPointer) {
-  this.dataPointer = dataPointer;
- }
-
- public int getDataPointer() {
-  return dataPointer;
- }
+    public void setData(int position, String data) {
+        mem.set(position, data);
+    }
+    
+    public int getDataPointer() {
+        return dataPointer;
+    }
 }
+
