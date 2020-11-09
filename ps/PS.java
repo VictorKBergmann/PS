0f1a664d25ff13ed94a620a677531486fa5092aa
Guilherme1999 <48689821+Guilherme1999@users.noreply.github.com>
2020-11-09 17:28:36 -0300

    Update PS.java

--- ps/PS.java
+++ ps/PS.java
@@ -1,26 +1,20 @@
+
 package ps;
-
-import gui.GUI;
-
-import java.awt.*;
-
+//import gui.GUI;
+//import java.awt.*;
 public class PS {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        Memory mem = new Memory();
-        //Loader loader = new Loader(mem);
+        Memory mem = new Memory(); //Precisa do Loader pra definir final da mem de instruções/inicio da mem de dados 
+       // Loader loader = new Loader("teste.txt", 13, mem);
+       // loader.loadAllInstructions();
         Cpu cpu = new Cpu();
-
-        String data;
         boolean signal = true;
 
         try{
-            while(signal){
-                data = cpu.read(mem);
-                System.out.println("\nPalavra que buscou no read: " + data);
-                signal = cpu.execute(data, mem); 
-                System.out.println("RE: " + cpu.getRe());
+            while(signal){      
+                signal = cpu.execute(mem); 
                 System.out.println("Acumulador: " + cpu.getAcc());
                 System.out.println("Memoria 44:" +mem.getData(44));
                 System.out.println("Memoria 31:" +mem.getData(31));
@@ -30,14 +24,13 @@ public class PS {
         catch(IllegalArgumentException e) {
             System.out.println("\nErro! Tentativa de acessar a memória de instruções.");
         }
-
-        /*EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                GUI gui = new GUI(mem, cpu);
-                gui.setVisible(true);
-            }
-        });*/
+            /*EventQueue.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    GUI gui = new GUI(mem, cpu);
+                    gui.setVisible(true);
+                }
+            });*/
     }
     
 }
