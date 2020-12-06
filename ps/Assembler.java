19bde79ab4edc790c0c6abf983ddb7c22cb09918
VictorKBergmann <vic.kunde@hotmail.com>
2020-12-06 11:56:55 -0300

    first and second Step

--- ps/Assembler.java
+++ ps/Assembler.java
@@ -1,132 +1,164 @@
 package ps;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Assembler {
-    private InputStreamReader input; //input's from interface
     private ArrayList<String> lines;
     private String line;
     private HashMap<String, Integer> symbolTable;
-    private HashMap<String, String> oppcodeTable;
-    //private HashMap<, String> pointerTable;
+    private Map<String, String> oppcodeTable;
+    private String[] aux;
 
- //00000000000 000 0000000000
-    public Assembler(){
+    private int locationCounter;
+
+    public Assembler(String adress){
         symbolTable = new HashMap<>();
         oppcodeTable = new HashMap<>();
-        //pointerTable = new HashMap<>();
         lines = new ArrayList<>();
-        //0000000000010 100
-        oppcodeTable.put("add", "010");
 
+        locationCounter = 0;
 
+        oppcodeTable = mapOppcode(oppcodeTable);
+
+        readFile(adress);
 
 
     }
 
     private void readFile(String adress){
-        try {
-            FileInputStream file = new FileInputStream(adress);
-            input = new InputStreamReader(file); //""
-        }
-        catch(Exception error){
-            System.out.println("Error on file reading "+ adress + " .");
-        }
+        try{
+            BufferedReader lerArq;
 
-        BufferedReader buffer = new BufferedReader(input);
-
-        try {
-            line = buffer.readLine();
-            while (lines != null) {
+            lerArq = new BufferedReader( new FileReader(adress));
+            line = lerArq.readLine();
+            while(line != null) {
                 lines.add(line);
-                line = buffer.readLine();
-
+                line = lerArq.readLine();
             }
+            lerArq.close();
         }
-        catch (IOException e) {
-            e.printStackTrace();
+        catch(IOException e) {
+            System.out.println("error on reading the file!");
         }
     }
 
-    private void map(){
-
-    }
 
     private void firstStep(String adress){
-        String[] aux;
-        int count = 0, i ;
-        int locationCounter = 0;
-        int opperatorscount = 0;
-        readFile(adress);
-        String oppcode, pointer = new String(), opp = new String();
-        ArrayList<String> byLine = new ArrayList<>();
+        String oppCode;
 
         for (String line: lines) {
 
 
             aux = line.split(" ");
-            label:
+            oppCode = oppcodeTable.get(getOperation(aux));// get oppCode
 
-            if (oppcodeTable.get(aux[count]) == null) {// get label
-                /*add on labels*/
-                symbolTable.put(aux[count], locationCounter);
-                locationCounter++; //TODO
-                count++;
+
+            if(!getLabel(aux).equals(null)) {
+                symbolTable.put(getLabel(aux), locationCounter);
             }
 
-            oppcode = oppcodeTable.get(aux[count]);// get oppCode
-            count++;
+            if(getOperation(aux).equals("end")){return;}
 
-
-            if (aux.length >= count) { // verify if have opp1
-                count++;
-
-                if (aux.length >= count) {// verify if have opp2
-                    count++;
-                    opperatorscount = 2;
-                } else {
-                    opperatorscount = 1;
-                }
-            }
-            if(opperatorscount == 2){// get pointer for 2 operand
-                pointer = adress(aux[aux.length - 1].charAt(0), aux[count - 2].charAt(0));
-            }
-            if(opperatorscount == 1){// get pointer for 1 operand
-                pointer = adress(aux[aux.length - 1].charAt(0));
-            }
-
-            for(;opperatorscount!=0; opperatorscount--){ // get operand
-                if(aux[aux.length - opperatorscount].charAt(0)== '#' || aux[aux.length - opperatorscount].charAt(0) == 'I') {// if has pointer
-                    opp.concat(aux[aux.length - opperatorscount].substring(1));
-                }
-                else if(isNumeric(aux[aux.length - opperatorscount])){ // if dont have pointer
-                    opp.concat(aux[aux.length - opperatorscount]);
-                }
-                else{ // if is label
-                    opp.concat("????????????????");
-                }
+            if(!oppCode.equals(null)){
+                locationCounter += getOperands(aux).size() + 1;
 
             }
-            pointer.concat(oppcode);
-            pointer.concat(opp);
-            byLine.add(pointer);
+            else if(true){/* verify psedo map TODO*/}
 
+            else{throw new IllegalArgumentException("instruction not found");}
         }
 
-
     }
-    String adress(char a1,char a2){// TODO
-        return "";
-    }String adress(char a1){// TODO
-        return "";
+
+    private void secondStep(){
+        ArrayList<String> byLine = new ArrayList<>();
+        String oppcode, pointer = new String(), opp = new String();
+        ArrayList<String> operands;
+
+        for (String line: lines) {
+            operands = getOperands(aux);
+
+            aux = line.split(" ");
+
+
+            pointer = adress(operands);
+            pointer.concat(oppcodeTable.get(getOperation(aux)));// get oppCode);
+
+            for (String operand: operands) {
+
+                if (operand.charAt(0) == '#' || operand.charAt(0) == 'I') {// if has pointer
+                    opp.concat(operand.substring(1));
+                }
+                else if (isNumeric(operand)) { // if dont have pointer
+                    opp.concat(operand);
+                }
+                else { // if is label
+                    opp.concat(Integer.toString(symbolTable.get(operand), 2));
+                }
+            }
+
+            pointer.concat(opp);
+            byLine.add(pointer);
+        }
+        generateObjectFile(byLine);
+    }
+
+    private void generateObjectFile(ArrayList<String> lins){
+        //genarate file TODO
+    }
+
+
+    String adress(ArrayList<String> operands){
+        if(operands.size() == 1){return "";}//TODO
+        else if(operands.size() == 2){return "";}//TODO
+        else return "000";
     }
     // # = imediato
     // I = indireto
     //   = direto
+
+    private String getLabel(String[] line) {
+        if(!line[0].equals("")){
+            return line[0];
+        }
+        return null;
+    }
+
+    private String getOperation(String[] line) {
+        return line[1];
+    }
+
+    private ArrayList<String> getOperands(String[] line) {
+        ArrayList<String> takeOperands = new ArrayList<>();
+        for(int i = 2; i < line.length; i++) {
+            takeOperands.add(line[i]);
+        }
+        return takeOperands;
+    }
+
+
+    private Map<String, String> mapOppcode(Map<String, String> pot) {
+        pot.put("br", "0000");
+        pot.put("brpos", "0001");
+        pot.put("add", "0010");
+        pot.put("load", "0011");
+        pot.put("brzero", "0100");
+        pot.put("brneg", "0101");
+        pot.put("sub", "0110");
+        pot.put("store", "0111");
+        pot.put("write", "1000");
+        pot.put("ret", "1001");
+        pot.put("divide", "1010");
+        pot.put("stop", "1011");
+        pot.put("read", "1100");
+        pot.put("copy", "1101");
+        pot.put("mult", "1110");
+        pot.put("call", "1111");
+        return pot;
+    }
+
 
     public static boolean isNumeric(String str) {
         try {
