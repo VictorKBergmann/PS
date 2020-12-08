4592636373cb876e8da9cd4b1aa6660044fbe332
Gustavo da Silva Machado <48689692+Guga15gu@users.noreply.github.com>
2020-12-08 13:25:07 -0300

    Update FormalParameterStack.java

--- ps/FormalParameterStack.java
+++ ps/FormalParameterStack.java
@@ -10,13 +10,44 @@ public class FormalParameterStack {
     public FormalParameterStack() {
     }
 
-    public void add(String name, int dPosition, int dLevel) {
-        Integer[] temp = new Integer[1];
-        temp[0]=dPosition;
-        temp[1]=dLevel;
+    public void add(String name, int dLevel, int dPosition) {
+        Integer[] temp = new Integer[2];
+        temp[0]=dLevel;
+        temp[1]=dPosition;
         this.definitionPositionAndLevelPair.add(temp);
         this.name.add(name);
     }
+    public void add(String name, int dPosition) {
+        Integer[] temp = new Integer[2];
+        temp[0]=getHighDLevel(name)+1;
+        temp[1]=dPosition;
+        this.definitionPositionAndLevelPair.add(temp);
+        this.name.add(name);
+    }
+    public int size (){
+        return name.size();
+    }
+    public int getHighDLevel(String parameter){
+
+        int index = name.lastIndexOf(parameter);
+        if(index == -1){
+            return 0;
+        }
+        else{
+            return definitionPositionAndLevelPair.get(index)[0];
+        }
+
+    }
+    public String getName(int index){
+        return this.name.get(index);
+    }
+    public int getDPosition(int index){
+        return definitionPositionAndLevelPair.get(index)[1];
+    }
+    public int getDLevel(int index){
+        return definitionPositionAndLevelPair.get(index)[0];
+    }
+
     public Integer[] getDefinitionPositionAndLevelPair(String name){
 
         for(int i = name.length()-1; i >= 0; --i){
